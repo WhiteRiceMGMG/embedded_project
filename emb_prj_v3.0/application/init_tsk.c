557c89c3721b3046deee6578b315c6c605b2117c
ogawa-vaio <3223mq118@gmail.com>
2025-10-21 22:11:14 +0900

    version 3.0

---
+++ emb_prj_v3.0/application/init_tsk.c
@@ -0,0 +1,41 @@
+/*********************************************************************************/
+/* Copyright OGAWA CONST                                                         */
+/*********************************************************************************/
+/* file : init_tsk.c                                                             */
+/* abst : init task                                                              */
+/* hist : 2025 / 10 /01                                                          */
+/*********************************************************************************/
+#include <trykernel.h>
+#include <knldef.h>
+
+void initsk(INT stacd, void *exinf);                /* 初期タスクの実行プログラム */
+UW  tskstk_ini[256/sizeof(UW)];                     /* 初期タスクのスタック       */
+ID  tskid_ini;                                      /* 初期タスクのID番号         */    
+
+T_CTSK  ctsk_ini = {
+    .tskatr     = TA_HLNG | TA_RNG0 | TA_USERBUF,   /* タスク属性                 */
+    .task       = initsk,                           /* タスクの実行関数           */
+    .itskpri    = 1,                                /* タスク優先度               */
+    .stksz      = sizeof(tskstk_ini),               /* スタックサイズ             */
+    .bufptr     = tskstk_ini,                       /* スタックへのポインタ       */
+};
+
+void initsk(INT stacd, void *exinf)
+{
+    tm_com_init();                                  /* シリアル通信の初期化       */
+    tm_putstring("Strat Try Kernel\n");
+
+    usermain();
+    tk_ext_tsk();
+}
+
+int main(void)
+{
+    tskid_ini = tk_cre_tsk(&ctsk_ini);              /* 初期タスク生成             */
+    tk_sta_tsk(tskid_ini, 0);                       /* 初期タスク実行             */
+     while(1);                                      /* ここは実行されない         */
+}
+
+/*********************************************************************************/
+/* EOF                                                                           */
+/*********************************************************************************/
