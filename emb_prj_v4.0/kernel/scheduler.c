a297448f95fc7965a279576cfa24a44cb1de32e6
ogawa <ocmikanmgmg@gmail.com>
2025-12-23 22:02:56 +0900

    a

--- emb_prj_v4.0/kernel/scheduler.c
+++ emb_prj_v4.0/kernel/scheduler.c
@@ -40,8 +40,33 @@ u4 disp_running;
 /* 引数   | なし                                        */
 /* 戻り値 | なし                                        */
 /********************************************************/
+void scheduler( void )
+{
+    INT i;
 
+    for(i = 0; i < CNF_MAX_TSKPRI; i++)
+    {
+        if(ready_queue[i] != NULL)
+        {
+            break;
+        }
+    }
 
+    if(i < CNF_MAX_TSKPRI)
+    {
+        sche_task = ready_queue[i];
+    }
+    else
+    {
+        sche_task = NULL;
+    }
+
+    if((sche_task != cur_task)
+      &&(!disp_running))
+    {
+        dispatch();
+    }
+}
 
 /********************************************************/
 /* EOF                                                  */
