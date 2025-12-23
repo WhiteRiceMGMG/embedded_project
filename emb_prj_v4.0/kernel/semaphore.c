4e2cf052a6464900447a1b542c56ee364fc04ebd
ogawa <ocmikanmgmg@gmail.com>
2025-12-23 22:32:20 +0900

    a

--- emb_prj_v4.0/kernel/semaphore.c
+++ emb_prj_v4.0/kernel/semaphore.c
@@ -1,7 +1,7 @@
-/* semaphore.c                                          */
+/* systimer.c                                           */
 /********************************************************/
-/* object    | セマフォ                                 */
-/* abstract  | セマフォ定義                             */
+/* object    | システムタイマ                           */
+/* abstract  | システムタイマ定義                       */
 /* edit his  | 2025/12/23 テンプレート作成              */
 /*           |                                          */
 /********************************************************/
@@ -20,159 +20,54 @@
 /* 外部公開変数                                         */
 /********************************************************/
 /* u1 u1g_sample_val                                    */
-SEMCB semcb_tbl[CNF_MAX_SEMID];
 
 /********************************************************/
 /* 内部公開変数                                         */
 /********************************************************/
-FLGCB flgvb_tbl[CNF_MAX_FLAGID];
+TCB *wait_queue;
 
 /********************************************************/
 /* 外部公開関数定義                                     */
 /********************************************************/
 
 /********************************************************/
-/* 関数   | ID tk_cre_sem( const T_CSEM *pk_csem )      */
-/* 説明   | セマフォ生成API                             */
-/* 引数   | T_CSEM *pk_csem                             */
-/* 戻り値 | semid                                       */
+/* 関数   | void systimer_handler( void )               */
+/* 説明   | シスタムタイマ割り込みハンドラ              */
+/* 引数   | なし                                        */
+/* 戻り値 | なし                                        */
 /********************************************************/
-ID tk_cre_sem( const T_CSEM *pk_csem )
+void systimer_handler( void )
 {
-    ID   semid;
-    UINT intsts; 
+    TCB *tcb, *next;
     
-    DI(intsts);
-    for(semid = 0; semcb_tbl[semid].state != KS_NONEXIST; 
-        semid++)
-
-    if(semid < CNF_MAX_SEMID)
+    for(tcb = wait_queue; tcb != NULL; tcb = next)
     {
-        semcb_tbl[semid].state  = KS_EXIST;
-        semcb_tbl[semid].semcnt = pk_csem -> isemcnt;
-        semcb_tbl[semid].maxsem = pk_csem -> maxsem;
-        semid++;
-    }
-    else
-    {
-        semid = E_LIMIT;
-    }
-    EI(intsts);
-    return semid;
-}
-
-/********************************************************/
-/* 関数   |ER tk_wai_sem( ID semid, INT cnt, TMO tmout )*/
-/* 説明   | セマフォの資源獲得API                       */
-/* 引数   | ID semid, INT cnt, TMO tmout                */
-/* 戻り値 | err                                         */
-/********************************************************/
-ER tk_wai_sem( ID semid, INT cnt, TMO tmout )
-{
-    SEMCB *semcb;
-    ER    err = E_OK;
-    UINT  intsts;
-
-    if(semid <= 0 || semid > CNF_MAX_SEMID)
-    {
-        return E_ID;
-    }
-    DI(intsts);
-    semcb = &semcb_tbl[--semid];
-    if(semcb -> state == KS_EXIST)
-    {
-        if(semcb -> semcnt >= cnt)
+        next = tcb -> next;
+        if(tcb -> waitim == TMO_FEVR)
         {
-            semcb -> semcnt -= cnt;
+            continue;
         }
-        else if(tmout == TMO_POL)
+        else if(tcb -> waitime > TIMER_PERIOD)
         {
-            err = E_MOUT;
+            tcb -> waitim -= TIMER_PERIOD;
         }
         else
         {
-            tqueue_remove_top
-                       (&ready_queue[cur_task -> itspri]);
-            cur_task -> state = TS_WAIT;
-            cur_task -> waifct = TWFCT_SEM;
-            cur_task -> waiobj = semid;
-            cur_task -> waitim = ((tmout == TMO_FEVR)
-                           ? tmout: tmout + TIMER_PERIOD);
-            cur_task -> waisem = cnt;
-            cur_task -> waierr = &err;
-
-            tqueue_add_entry(&wait_queue, cur_task);
-            scheduler();
-        }
-    }
-    else
-    {
-        err = E_NOEXS;
-    }
-
-    EI(intsts);
-    return err;
-}
-
-/********************************************************/
-/* 関数   | ER tk_sig_sem( ID semid, INT cnt )          */
-/* 説明   | セマフォの資源返却API                       */
-/* 引数   | ID semid, INT cnt                           */
-/* 戻り値 | ER                                          */
-/********************************************************/
-ER tk_sig_sem( ID semid, INT cnt )
-{
-    SEMCB *semcb;
-    TCB   *tcb;
-    ER    err = E_OK;
-    UINT  intsts;
-
-    if(semid <= 0 || semid > CNF_MAX_SEMID)
-    {
-        return E_ID;
-    }
-    DI(intsts);
-    semcb = &semcb_tbl[--semid];
-    if(semcb -> state == KS_EXIST)
-    {
-        semcb -> semcnt += cnt;
-        if(semcb -> semcnt <= semcb -> maxsem)
-        {
-            for(tcb = wait_queue; tcb != NULL; tcb = tcb -> next)
+            tqueue_remove_enrty(&wait_queue, tcb);
+            if(tcb -> waitfct = TWFCT_DLY)
             {
-                if((cb->waifct == TWFCT_SEM)
-                 &&(tcb -> waiobj == semid))
-                {
-                    if(semcb -> semcnt >= tcb -> waisem)
-                    {
-                        semcb -> semcnt -= tcb -> waisem;
-                        tqueue_remove_enrty(&wait_queue
-                                            , tcb);
-                        tcb -> state  = TS_READY;
-                        tcb -> waifct = TWFCT_NOW;
-                        tcb -> waierr = &err;
-                        tqueue_remove_enrty(&ready_queue[tcb -> itskpri], tcb);
-                        scheduler();
-                    }
-                    else
-                    {
-                        break;
-                    }
-                }
+                *tcb -> waierr = E_OK;
             }
-        }
-        else
-        {
-            semcb -> semcnt -= cnt;
-            err = E_QOVR;
-        }
+            else
+            {
+                *tcb -> waierr = E_TMOUT;
+            }
+            tcb -> state  = TS_READY;
+            tcb -> waifct = TWFCT_NOW;
+            tqueue_add_enrty(&ready_queue[tcb -> itspri], tcb);
+       }
     }
-    else
-    {
-        err = E_NOEXS;
-    }
-    EI(intsts);
-    return err;
+    scheduler();
 }
 
 /********************************************************/
