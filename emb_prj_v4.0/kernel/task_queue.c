4cd547ece056248c1b804c05a6eeebcbbde7e265
ogawa <ocmikanmgmg@gmail.com>
2026-01-02 04:39:16 +0900

    a

--- emb_prj_v4.0/kernel/task_queue.c
+++ emb_prj_v4.0/kernel/task_queue.c
@@ -21,7 +21,23 @@
 /* 引数   | TCB **queue, TCB *tcb                       */
 /* 戻り値 | semid                                       */
 /********************************************************/
-後は関数本体書くだけ
+void tqueue_add_enrty( TCB **queue, TCB *tcb )
+{
+    TCB *queue_end;
+    if(*queue == NULL)
+    {
+        *queue     = tcb;
+        tcb -> pre = tcb;
+    }
+    else
+    {
+        queue_end         = (*queue) -> pre;
+        queue_end -> next = tcb;
+        tcb -> pre        = queue_end;
+        (*queue) -> pre   = tcb;
+    }
+    tcb -> next = NULL;
+}
 
 /********************************************************/
 /* 関数   | void tqueue_remove_top( TCB **queue )       */
@@ -29,7 +45,20 @@
 /* 引数   | TCB **queue                                 */
 /* 戻り値 | err                                         */
 /********************************************************/
-後は関数本体書くだけ
+void tqueue_remove_top( TCB **queue )
+{
+    TCB *top;
+    if(*queue == NULL)
+    {
+        return;
+    }
+    top = *queue;
+    *queue = top -> next;
+    if(*queue != NULL)
+    {
+        (*queue) -> pre = top -> pre;
+    }
+}
 
 /********************************************************/
 /* 関数   | void tqueue_remove_entry (TCB **queue,      */
@@ -38,7 +67,26 @@
 /* 引数   | TCB **queue, TCB *tcb                       */
 /* 戻り値 | なし                                        */
 /********************************************************/
-後は関数本体書くだけ
+void tqueue_remove_enrty( TCB **queue, TCB *tcb )
+{
+    if(*queue == tcb)
+    {
+        tqueue_remove_top(queue);
+
+    }
+    else
+    {
+        (tcb -> pre) -> next = tcb -> next;
+        if(tcb -> next != NULL)
+        {
+            (tcb -> next) -> pre = tcb -> pre;
+        }
+        else
+        {
+            (*queue) -> pre = tcb -> pre;
+        }
+    }
+}
 
 /********************************************************/
 /* EOF                                                  */
